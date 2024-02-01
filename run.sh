db2e3d75556c0af61a7dfb8b796817bc53a8bfd5
rmoff <robin@decodable.co>
2024-02-01 19:22:10 +0000

    Enable INFO logging

--- run.sh
+++ run.sh
@@ -6,4 +6,4 @@ set -euxo pipefail
 /opt/hive-metastore/bin/schematool --verbose -dbType derby -initSchema
 
 # start metastore (in foreground)
-/opt/hive-metastore/bin/hive --service metastore
+/opt/hive-metastore/bin/hive --service metastore --hiveconf hive.root.logger=INFO,console
